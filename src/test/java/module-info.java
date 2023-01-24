6d1fb2cb167711b2329225644d0b274c90916297
Joran Wolken <joran@nubilus.nl>
2023-01-24 15:54:30 +0100

    With coverage

---
+++ src/test/java/module-info.java
@@ -0,0 +1,18 @@
+open module mollie
+{
+    requires com.fasterxml.jackson.databind;
+    requires com.fasterxml.jackson.core;
+    exports nl.nubilus.mollie;
+    exports nl.nubilus.mollie.payment;
+
+    requires org.mockito;
+    requires org.mockito.junit.jupiter;
+    requires net.bytebuddy.agent;
+    requires net.bytebuddy;
+    requires org.assertj.core;
+    requires transitive org.junit.jupiter.engine;
+    requires transitive org.junit.jupiter.api;
+    requires wiremock.jre8.standalone;
+
+    exports nl.nubilus.mollie.api;
+}
